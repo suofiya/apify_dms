8e67ad855813ea9089fb69bdeda175d323f70bc3
Brother Tao <manlianxiao3@126.com>
2013-03-07 15:16:23 +0800

    remove invalid php code

--- wp_content/themes/apify-dms/index.php
+++ wp_content/themes/apify-dms/index.php
@@ -7,7 +7,7 @@ $img = get_stylesheet_directory_uri().'/images/home_banner.jpg';
 $img1 = get_stylesheet_directory_uri().'/images/slide_1.jpg';
 $img2 = get_stylesheet_directory_uri().'/images/slide_2.jpg';
 $img3 = get_stylesheet_directory_uri().'/images/slide_3.jpg';
-echo do_shortcode('[jobsNarrowSearch bannerimages="' . $img1 . '|' . $img2 . '|' .$img3 . '"]');
+//echo do_shortcode('[jobsNarrowSearch bannerimages="' . $img1 . '|' . $img2 . '|' .$img3 . '"]');
 ?>
 </div>
 <?php get_footer(); ?>
