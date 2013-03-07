66514420a064530027e937e24e94024103c50810
Brother Tao <manlianxiao3@126.com>
2013-03-07 18:31:39 +0800

    add api docmt index frontend page

---
+++ wp_content/plugins/apify-dms/includes/pages/frontend/display_api_doc_index.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * 前台显示api文档首页内容
+ *
+ *
+ * @package    includes/pages/
+ * @subpackage frontend
+ *
+ * @copyright Copyright (c) 2013, lightinthebox.com
+ * @version $Id$
+ * @author liutao@lighitnthebox.com
+ */
+?>
+
+<!-- main begin -->
+<div class="col-main bg_line">
+	<div class="contentlist-front" style="width: 825px;margin: 0 auto;">
+		<h1 class="title">文档中心xxx</h1>
+		<ul class="front-content layout">
+			<li class="contentlist-item odd">
+				<div>
+					<div class="head-title  clearfix">
+						<h2 class="sub-title">平台介绍</h2>
+						<a class="link info-more" href="http://open.tbox.me/doc/api-group-list"> 了解更多</a>
+					</div>
+					从开发业务、应用类型等方面对Vela开放平台进行了详尽的介绍，是开发者加入开放平台前的入门教程。
+				</div>
+			</li>
+			<li class="contentlist-item">
+				<div>
+					<div class="head-title  clearfix">
+						<h2 class="sub-title">开发文档</h2>
+						<a class="link info-more" href="http://open.tbox.me/doc/api-group-list"> 了解更多</a>
+					</div>
+					开发文档为开发者提供API文档、API更新历史、新手指南、开发基础篇、开发进阶篇等内容。
+				</div>
+			</li>
+		</ul>
+		
+		<ul class="front-content layout">
+			<li class="contentlist-item odd">
+				<div>
+					<div class="head-title  clearfix">
+						<h2 class="sub-title">沙盒测试</h2>
+						<a class="link info-more" href="sandbox-api.lightinthebox.com/api/tools/api_tool.php"> 了解更多</a>
+					</div>
+					沙盒测试环境为开发者提供API接口测试，API参数说明等内容。
+				</div>
+			</li>
+			<li class="contentlist-item">
+				<div>
+					<div class="head-title clearfix">
+						<h2 class="sub-title">常见问题</h2>
+						<a class="link info-more" href="http://open.tbox.me/doc/api-group-list"> 了解更多</a>
+
+					</div>	
+					常见问题为开发者提供开发相关、规则相关的常见问题。
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+<!-- main end -->
